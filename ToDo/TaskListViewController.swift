65a3b36c4d2cb5569c94b313280e222f2af25e2c
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-19 22:55:46 +0400

    initial commit

---
+++ ToDo/TaskListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Saba Gogrichiani on 19.02.24.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
